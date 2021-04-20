5c871a1d576e03a33eb7168ced03b84b4399bd05
atsukikakiuchi <akak92000@gmail.com>
2021-04-20 15:54:00 +0900

    修正２

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
-      redirect_to prototype_path(@comment.prototype)
+      redirect_to "prototypes/show"
     else
       @prototype = @comment.prototype
       @comments = @prototype.comments
@@ -10,7 +10,7 @@ class CommentsController < ApplicationController
     end
   end
   private
-  
+
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
   end
