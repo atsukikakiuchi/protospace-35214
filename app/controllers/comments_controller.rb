37f55b2162a336a547e234c960f19022a5afc940
atsukikakiuchi <akak92000@gmail.com>
2021-04-20 15:30:17 +0900

    修正１

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,9 +9,8 @@ class CommentsController < ApplicationController
       render "prototypes/show"
     end
   end
-
   private
-
+  
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
   end
